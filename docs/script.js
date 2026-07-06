44a3a43b3907e0a9425fd8ab79934460d92bab3f
Bobface87 <petrik.robo@gmail.com>
2026-07-06 12:45:26 +0200

    verzia 1.0 - frontend

--- docs/script.js
+++ docs/script.js
@@ -1,50 +1,51 @@
-async function loadMenu() {
-  const res = await fetch("menu.json");
-  const data = await res.json();
 
-  render(data);
+async function loadMenu() {
+	const res = await fetch("menu.json");
+	const data = await res.json();
+
+	render(data);
 }
 
 function render(data) {
-  const app = document.getElementById("app");
-  const search = document.getElementById("search");
+	const app = document.getElementById("app");
+	const search = document.getElementById("search");
 
-  function draw(filter = "") {
-    app.innerHTML = "";
+	function draw(filter = "") {
+		app.innerHTML = "";
 
-    data.forEach(r => {
-      let meals = r.meals.filter(m =>
-        m.name.toLowerCase().includes(filter.toLowerCase())
-      );
+		data.forEach(r => {
+			let meals = r.meals.filter(m =>
+				m.name.toLowerCase().includes(filter.toLowerCase())
+			);
 
-      if (meals.length === 0) return;
+			if (meals.length === 0) return;
 
-      let html = `
-        <div class="card">
-          <h2>🍽 ${r.restaurant}</h2>
-          <p>🥣 ${r.soup}</p>
-      `;
+			let html = `
+				<div class="card">
+					<h2>🍽 ${r.restaurant}</h2>
+					<p>🥣 ${r.soup}</p>
+			`;
 
-      meals.forEach(m => {
-        html += `
-          <div>
-            ${m.name}
-            <span class="price">${m.price.toFixed(2)} €</span>
-          </div>
-        `;
-      });
+			meals.forEach(m => {
+				html += `
+					<div>
+						${m.name}
+						<span class="price">${m.price.toFixed(2)} €</span>
+					</div>
+				`;
+			});
 
-      html += `</div>`;
+			html += `</div>`;
 
-      app.innerHTML += html;
-    });
-  }
+			app.innerHTML += html;
+		});
+	}
 
-  search.addEventListener("input", e => {
-    draw(e.target.value);
-  });
+	search.addEventListener("input", e => {
+		draw(e.target.value);
+	});
 
-  draw();
+	draw();
 }
 
 loadMenu();
