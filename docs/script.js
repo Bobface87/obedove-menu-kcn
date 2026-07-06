c7ea4021f59add49955fadcd45ce1bec5a6710bb
Bobface87 <petrik.robo@gmail.com>
2026-07-06 12:33:10 +0200

    verzia 1.0 - frontend

---
+++ docs/script.js
@@ -0,0 +1,50 @@
+async function loadMenu() {
+  const res = await fetch("menu.json");
+  const data = await res.json();
+
+  render(data);
+}
+
+function render(data) {
+  const app = document.getElementById("app");
+  const search = document.getElementById("search");
+
+  function draw(filter = "") {
+    app.innerHTML = "";
+
+    data.forEach(r => {
+      let meals = r.meals.filter(m =>
+        m.name.toLowerCase().includes(filter.toLowerCase())
+      );
+
+      if (meals.length === 0) return;
+
+      let html = `
+        <div class="card">
+          <h2>🍽 ${r.restaurant}</h2>
+          <p>🥣 ${r.soup}</p>
+      `;
+
+      meals.forEach(m => {
+        html += `
+          <div>
+            ${m.name}
+            <span class="price">${m.price.toFixed(2)} €</span>
+          </div>
+        `;
+      });
+
+      html += `</div>`;
+
+      app.innerHTML += html;
+    });
+  }
+
+  search.addEventListener("input", e => {
+    draw(e.target.value);
+  });
+
+  draw();
+}
+
+loadMenu();
